5fefe66383edeb8c49a7aaea3e8ac2d0db5ab82c
igor_alves <igornalves08@gmail.com>
2024-10-17 00:28:03 -0300

    criando a docs do projeto para visializar a parte das rotas da aplicacao

--- src/main/java/com/christoffer/apiimages/ApiImagesApplication.java
+++ src/main/java/com/christoffer/apiimages/ApiImagesApplication.java
@@ -3,12 +3,15 @@ package com.christoffer.apiimages;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Legacy AI API",description = "API para criacao de um Chat com inteeligencia artificial", version = "1.5"))
 public class ApiImagesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiImagesApplication.class, args);
-
     }
 
 }
